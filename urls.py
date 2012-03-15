609fe5bfbcdada3434395190eb515cd3462908bf
kurdd <prodmand@gmail.com>
2012-03-15 22:01:24 +0400

    changed bootstrap search added

--- urls.py
+++ urls.py
@@ -1,17 +1,21 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.core.urlresolvers import reverse
 from django.conf import settings
 from django.contrib import admin
 from main.views import *
+from main.forms import *
+from haystack.query import SearchQuerySet
+from haystack.views import SearchView
 
 admin.autodiscover()
+sqs = SearchQuerySet()
 
 urlpatterns = patterns('',
-                       url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     (r'^$', main),
     (r'^add/', add),
     (r'^contact/', contact),
     (r'^([A-Z]{1})/', letter),
+    (r'^number/$', number),
     (r'^(\d{1,10})/$', artist),
     (r'^(\d{1,10})/(\d{1,10})/$', album),
     (r'^(\d{1,10})/(\d{1,10})/(\d{1,10})/$', song),
@@ -20,4 +24,10 @@ urlpatterns = patterns('',
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_URL}),
     (r'^albums/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.ALBUMS_URL}),
     (r'^artists/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.ARTISTS_URL}),
-                       )
+    (r'^search/', SearchView(
+        template='search/search.html',
+        searchqueryset=sqs,
+        form_class=SimpleSearchForm,
+        results_per_page=None
+    )),
+)
